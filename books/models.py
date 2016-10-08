6374a7c6a731ca0e438dba6194c4ee9cca73c5da
ktenman <ktenman@users.noreply.github.com>
2016-10-08 20:30:36 +0300

    Initialize book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    comment = models.TextField(blank=True, null=True)
+    date_commented = models.DateField(blank=True, null=True)
+    is_bookmarked = models.BooleanField(default=False)
