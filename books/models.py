345381a094e0f953e9f7cf4f88cc4ddc473f6074
ktenman <ktenman@users.noreply.github.com>
2016-10-09 13:33:09 +0300

    add some help text and show Books in admin page

--- books/models.py
+++ books/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, help_text="Maximum 200 letters")
+    author = models.CharField(max_length=100, help_text="Maximum 100 letters")
     comment = models.TextField(blank=True, null=True)
     date_commented = models.DateField(blank=True, null=True)
-    is_bookmarked = models.BooleanField(default=False)
+    is_bookmarked = models.BooleanField(default=False, verbose_name="Favourite?")
 
     def __str__(self): # adding toString() method to represent objects
         return ("{title = '%s', author = '%s'}"% (self.title, self.author))
