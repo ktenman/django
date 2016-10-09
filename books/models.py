04fc2dd82a1452739d88ddf6aa1e5e7147770211
ktenman <ktenman@users.noreply.github.com>
2016-10-09 22:23:40 +0300

    improved models

--- books/models.py
+++ books/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.timezone import now
 
 class Book(models.Model):
     title = models.CharField(max_length=200, help_text="Maximum 200 letters")
     authors = models.ManyToManyField("Author", related_name="books")
     comment = models.TextField(blank=True, null=True)
-    date_commented = models.DateField(blank=True, null=True)
+    date_commented = models.DateTimeField(blank=True, null=True)
     is_bookmarked = models.BooleanField(default=False, verbose_name="Is favourite?")
 
     def __str__(self):
-        return self.title
+        return "{} by {}".format(self.title, self.list_authors())
+
+    def list_authors(self):
+        return ", ".join([author.name for author in self.authors.all()])
+
+    def save(self, *args, **kwargs):  # overide django built-in 'save' method
+        if self.comment and self.date_commented is None: # has comment and no date
+            self.date_commented = now()
+
+        super(Book, self).save(*args, **kwargs)
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100, help_text="Maximum 100 letters", unique=True)
