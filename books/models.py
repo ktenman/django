52ee17bb846e598e57680fbae41ae481ef6f420b
ktenman <ktenman@users.noreply.github.com>
2016-10-09 16:00:17 +0300

    adding many-to-many relations

--- books/models.py
+++ books/models.py
@@ -4,11 +4,16 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200, help_text="Maximum 200 letters")
-    author = models.CharField(max_length=100, help_text="Maximum 100 letters")
+    authors = models.ManyToManyField("Author", related_name="books")
     comment = models.TextField(blank=True, null=True)
     date_commented = models.DateField(blank=True, null=True)
     is_bookmarked = models.BooleanField(default=False, verbose_name="Is favourite?")
 
-    def __str__(self): # adding toString() method to represent objects
-        return ("{title = '%s', author = '%s'}"% (self.title, self.author))
+    def __str__(self):
+        return self.title
 
+class Author(models.Model):
+    name = models.CharField(max_length=100, help_text="Maximum 100 letters", unique=True)
+
+    def __str__(self):
+        return self.name
