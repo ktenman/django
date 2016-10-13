0d38bca5cd745cf11f0307e1233d299917a7374e
ktenman <ktenman@users.noreply.github.com>
2016-10-13 13:32:56 +0300

    book.html / book-detail

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Count
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import View, DetailView
 
 from books.models import Book, Author
 
@@ -15,10 +16,24 @@ def list_books(request):
 class AuthorList(View):
     def get(self , request):
 
-        authors = Author.objects.all()
+        authors = Author.objects.annotate(
+            published_books=Count('books')
+        ).filter(
+            published_books__gt=0
+        )
 
         context = {
             'authors': authors
         }
 
         return render(request, "authors.html", context)
+
+
+class BookDetail(DetailView):
+    model = Book
+    template_name = "book.html"
+
+
+class AuthorDetail(DetailView):
+    model = Author
+    template_name = "author.html"
