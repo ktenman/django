2865755927218435d4b64e77e8aa8858abf095bd
Konstantin Tenman <konstantin.tenman@knowit.ee>
2017-02-14 21:56:49 +0200

    Raamatute haldus administraatori paneeli alt.

--- books/models.py
+++ books/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, help_text="Maximum 200 letters")
+    author = models.CharField(max_length=100, help_text="Maximum 100 letters")
     comment = models.TextField(blank=True, null=True)
     date_commented = models.DateField(blank=True, null=True)
-    is_bookmarked = models.BooleanField(default=False)
+    is_bookmarked = models.BooleanField(verbose_name="Is favourite?", default=False)
 
-    def __str__(self):
-        return self.title
+    def __str__(self):  # represents object with title and author
+        return "title = '%s', author = '%s'" % (self.title, self.author)
