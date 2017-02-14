1e2d9cdb937d4631f8c2205365354583c5f566e9
Konstantin Tenman <konstantin.tenman@knowit.ee>
2017-02-14 22:14:48 +0200

    Mitu mitmele seose loomine. Tehakse kaks tabelit, üks autorite ja teine raamatute jaoks. Raamatute tabelis on defineeritud mitu mitmele seos.

--- books/models.py
+++ books/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.utils.datetime_safe import datetime
 
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200, help_text="Maximum 200 letters")
-    author = models.CharField(max_length=100, help_text="Maximum 100 letters")
+    authors = models.ManyToManyField("Author", related_name="books")
     comment = models.TextField(blank=True, null=True)
-    date_commented = models.DateField(blank=True, null=True)
+    date_commented = models.DateTimeField(default=datetime.now, blank=True)
     is_bookmarked = models.BooleanField(verbose_name="Is favourite?", default=False)
 
-    def __str__(self):  # represents object with title and author
-        return "title = '%s', author = '%s'" % (self.title, self.author)
+    def __str__(self):
+        return "%s by %s" % (self.title, self.list_authors())
+
+    def list_authors(self):
+        return ", ".join([author.name for author in self.authors.all()])
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, help_text="Maximum 100 letters", unique=True)
+
+    def __str__(self):
+        return self.name
