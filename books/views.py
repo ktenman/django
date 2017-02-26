4bf0fc5dc09f4612b511026ef31e71b685720606
Konstantin Tenman <konstantin.tenman@knowit.ee>
2017-02-27 00:20:19 +0200

    Klassipõhine vaade nende autorite kuvamiseks, kellel on vähemalt 1 raamat.

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-from books.models import Book
+from django.views import View
+from books.models import Book, Author
+from django.db.models import Count
 
 
 def list_books(request):
@@ -11,3 +12,19 @@ def list_books(request):
     }
 
     return render(request, "list.html", context)
+
+
+class AuthorList(View):
+    @staticmethod
+    def get(request):
+        authors = Author.objects.annotate(
+            published_books=Count('books')
+        ).filter(
+            published_books__gt=0
+        )
+
+        context = {
+            'authors': authors
+        }
+
+        return render(request, "authors.html", context)
